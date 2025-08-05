a3e073484df2b60fc883dfad1b655eb54a4df0a9
= <=>
2025-08-06 02:10:49 +0300

    fixed cfg

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  reactStrictMode: true,
+  distDir: "dist",
+  devIndicators: false,
   images: {
     domains: ["31.172.66.97", "http://31.172.66.97/"],
     unoptimized: true,
   },
-  distDir: "dist",
 };
 
 export default nextConfig;
