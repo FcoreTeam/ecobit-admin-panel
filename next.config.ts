f157044172e7e84307b68184467f9b87854fc4e9
thefankor <82701509+thefankor@users.noreply.github.com>
2025-08-06 02:29:18 +0300

    fix: updated next.config.ts - remove output export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   images: {
     domains: ["31.172.66.97", "http://31.172.66.97/"],
     unoptimized: true,
